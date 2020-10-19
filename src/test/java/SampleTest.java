1a861f1270b1c268519605b2992b0c5511abbfaf
leonardjors <67611348+leonardjors@users.noreply.github.com>
2020-10-19 17:40:41 +0300

    Revert "WebDriverFactory was added"

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,12 +1,14 @@
 import config.ServerConfig;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class SampleTest {
@@ -14,16 +16,11 @@ public class SampleTest {
     private static WebDriver driver;
     private Logger logger = LogManager.getLogger(SampleTest.class);
     private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
-    //private String browserName = System.getProperty("Dbrowser");
 
-    @BeforeMethod
+    @Before
     public void setUp() {
-        String browserName = System.getProperty("browser");
-        //if (browserName == null) {
-         //   browserName = "Chrome";
-       // }
-
-        driver = WebDriverFactory.create(browserName);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
         logger.info("Драйвер поднят");
     }
 
@@ -43,7 +40,7 @@ public class SampleTest {
 
     }
 
-    @AfterMethod
+    @After
     public void setDown() {
         if (driver != null) {
             driver.quit();
